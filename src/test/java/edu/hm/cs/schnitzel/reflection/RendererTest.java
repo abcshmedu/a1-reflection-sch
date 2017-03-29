2865c8d4b8258b2ad14380bbc533d6dd23114c64
truenicfel <dassler@hm.edu>
2017-03-29 14:58:32 +0200

    fixed formatting

--- src/test/java/edu/hm/cs/schnitzel/reflection/RendererTest.java
+++ src/test/java/edu/hm/cs/schnitzel/reflection/RendererTest.java
@@ -6,19 +6,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RendererTest {
-	private ExampleClass toRender;
-	private Renderer renderer;
 
-	@Before
-	public void setUp() {
-		toRender = new ExampleClass(5);
-		renderer = new Renderer(toRender);
-	}
+    private ExampleClass toRender;
+    private Renderer renderer;
 
-	 @Test
-	 public void testRendering() throws Exception {
-	 assertEquals("Instance of edu.hm.SomeClass:\n" +
-				"foo (Type int): 5\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-				renderer.render());
-	}
+    @Before
+    public void setUp() {
+        toRender = new ExampleClass(5);
+        renderer = new Renderer(toRender);
+    }
+
+    @Test
+    public void testRendering() throws Exception {
+        assertEquals("Instance of edu.hm.SomeClass:\n"
+                + "foo (Type int): 5\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                renderer.render());
+    }
 }
