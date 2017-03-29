707e19e3e4dab29ebad0ae865471429d9c750563
konopac <konopac@hm.edu>
2017-03-29 13:54:28 +0200

    maven project created

---
+++ src/main/java/edu/hm/cs/schnitzel/reflection/App.java
@@ -0,0 +1,13 @@
+package edu.hm.cs.schnitzel.reflection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
