c06681fea528f1bfc30056f25e6866091d4e0e22
truenicfel <dassler@hm.edu>
2017-03-29 14:32:28 +0200

    added interface Render me + basic example class to test functionality

---
+++ src/main/java/edu/hm/cs/schnitzel/reflection/ExampleClass.java
@@ -0,0 +1,19 @@
+package edu.hm.cs.schnitzel.reflection;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nicfel
+ */
+public class ExampleClass {
+
+    @RenderMe private final int foo;
+    @RenderMe(with = "edu.hm.renderer.ArrayRenderer")int[] array = {1, 2, 3,};
+    @RenderMe private final Date date = new Date(123456789);
+
+    public ExampleClass(int foo) {
+        this.foo = foo;
+    }
+
+}
