de6a9e0ef7614a80fb59ac1ad8b8115017a2ed72
truenicfel <dassler@hm.edu>
2017-04-04 11:03:36 +0200

    started implementation of Renderer

--- src/main/java/edu/hm/cs/schnitzel/reflection/RenderMe.java
+++ src/main/java/edu/hm/cs/schnitzel/reflection/RenderMe.java
@@ -1,9 +1,13 @@
 package edu.hm.cs.schnitzel.reflection;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  *
  * @author nicfel
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RenderMe {
 
     public String with() default "edu.hm.cs.schnitzel.reflection.Renderer";
--- src/main/java/edu/hm/cs/schnitzel/reflection/Renderer.java
+++ src/main/java/edu/hm/cs/schnitzel/reflection/Renderer.java
@@ -1,5 +1,10 @@
 package edu.hm.cs.schnitzel.reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Renderer {
 
     private final Object object;
@@ -10,12 +15,31 @@ public class Renderer {
 
     public String render() {
 
-        
+        final Class<?> toRender = getObject().getClass();
+        final Field[] fields = toRender.getFields();
         //loop over object variables of object
-            
+        for (final Field field : fields) {
             //check annotations == renderMe
-            //if annotation has param -> call param rendererer method
-            //else just render the object
+            RenderMe renderMe = field.getAnnotation(RenderMe.class);
+            if(renderMe != null) {
+                try {
+                    //if annotation has param -> call param rendererer method
+                    String path = renderMe.with();
+                    Class<?> renderClass = Class.forName(path); 
+                    if(renderClass != this.getClass()) {
+                        
+                        renderClass.getConstructor(getObject());
+                        
+                    } else {
+                        //else just render the object
+                        
+                    }
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("ERRRRor");
+                }
+            }
+
+        }
 
         return "";
     }
